7946aefd0246658e7a12de9ebca6d3f71f8a7c58
Ben Bigelow <beb601@mail.harvard.edu>
2018-01-07 22:00:55 -0500

    Finish Chapter 2

--- Chapter2.R
+++ Chapter2.R
@@ -44,5 +44,49 @@ hist(college$Room.Board,breaks=10)
 
 ## 9.
 ## a)
+auto=read.csv("Auto.csv")
+summary(auto)
+sapply(auto,is.numeric)
 
 ## b)
+sapply(auto[,sapply(auto,is.numeric)],range)
+
+## c)
+sapply(auto[,sapply(auto,is.numeric)],mean)
+sapply(auto[,sapply(auto,is.numeric)],sd)
+
+## d)
+sapply(auto[-c(10:85),sapply(auto,is.numeric)],range)
+sapply(auto[-c(10:85),sapply(auto,is.numeric)],mean)
+sapply(auto[-c(10:85),sapply(auto,is.numeric)],sd)
+
+## e)
+pairs(auto)
+
+## 10.
+## a)
+library(MASS)
+?Boston
+
+## b)
+pairs(Boston)
+
+## c)
+## d)
+sapply(Boston[,sapply(Boston,is.numeric)],range)
+
+## e)
+table(Boston$chas)
+
+## f)
+median(Boston$ptratio)
+
+## g)
+which.min(Boston$medv)
+Boston[which.min(Boston$medv),]
+sapply(Boston[,sapply(Boston,is.numeric)],range)
+
+## h)
+dim(Boston[Boston$rm>7,])
+dim(Boston[Boston$rm>8,])
+summary(Boston[Boston$rm>8,])
